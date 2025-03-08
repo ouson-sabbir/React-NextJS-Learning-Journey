fdc177fd8f590b908a5de50be77fd096c4eb71fb
ouson-sabbir <ousonsabbir4@gmail.com>
2025-03-09 00:49:54 +0600

    added installation

---
+++ learning-lessons/lesson-one/my-next-app/app/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+    const teamOne = "India";
+    const teamTwo = "Newzealand";
+
+    return (
+      <>
+       <h1 className='w-100 font-bold text-yellow-400 h-100 flex items-center bg-amber-950'> Tomorrow Final Match between {teamOne} Vs {teamTwo}</h1>
+      </>
+    )
+}
+
+export default page
