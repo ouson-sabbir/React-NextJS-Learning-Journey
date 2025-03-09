3f254fbbe260a41d522e3a5563247b776da7195e
ouson-sabbir <ousonsabbir4@gmail.com>
2025-03-09 22:53:38 +0600

    add use state

--- learning-lessons/lesson-one/my-next-app/app/page.js
+++ learning-lessons/lesson-one/my-next-app/app/page.js
@@ -1,14 +1,27 @@
-import React from 'react'
+"use client";
 
-const page = () => {
-    const teamOne = "India";
-    const teamTwo = "Newzealand";
+import React, { useState } from "react";
 
-    return (
-      <>
-       <h1 className='w-100 font-bold text-yellow-400 h-100 flex items-center bg-amber-950'> Tomorrow Final Match between {teamOne} Vs {teamTwo}</h1>
-      </>
-    )
-}
+const Page = () => {
+  const [mark, setMarks] = useState(80);
 
-export default page
+  const markChange = () => {
+    setMarks(90);
+  };
+
+  return (
+    <>
+      <h1 className="w-100 font-bold text-yellow-400 h-100 flex items-center bg-amber-950">
+        My Mark is {mark}
+      </h1>
+      <button
+        onClick={markChange}
+        className="rounded-sm p-3 bg-black text-white font-medium"
+      >
+        Click Me
+      </button>
+    </>
+  );
+};
+
+export default Page;
